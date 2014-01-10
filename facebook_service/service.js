e494381ef99180dbe5417e616622b189e17c667b
tor <torkale@gmail.com>
2014-01-11 00:59:50 +0200

    small refactoring of node and ruby

--- facebook_service/service.js
+++ facebook_service/service.js
@@ -3,24 +3,21 @@ var http = require('http');
 
 var amqp = require('amqp');
 
-var connection = amqp.createConnection();
-
 var connection = amqp.createConnection({url:"amqp://guest:guest@localhost:5672"});
-// Wait for connection to become established.
 connection.on('ready', function () {
-  // Use the default 'amq.topic' exchange
-  connection.queue('location', function(q){
-      // Catch all messages
-      q.bind('#');
+  connection.queue('location', {durable: true}, function(q){
+      q.bind('location');
   });
 });
 console.log("Starting server on port 8080")
 
 var app = function(request,res){
   var body = '';
+
   request.on('data', function (data) {
     body += data;
   });
+
   request.on('end', function () {
     var user = JSON.parse(body);
     connection.publish(user.entry[0].changed_fields[0], JSON.stringify(user));
--- facebook_service/service.rb
+++ facebook_service/service.rb
@@ -2,20 +2,20 @@ require "rubygems"
 require 'sinatra'
 require 'json'
 require 'bunny'
-PORT = 8080
-puts "Starting server on port #{PORT}"
+
+set :port, 8080
+puts "Starting server on port 8080"
+
 connection = Bunny.new
 connection.start
-
 channel  = connection.create_channel
-# topic exchange name can be any string
 exchange = channel.topic("facebook_events", :durable => true)
 channel.queue("location", :durable => true).bind(exchange, :routing_key => "location")
 
 get '/event' do
   raw = request.body.read
   data = JSON.parse raw
-  data["entry"].each do |entry| 
+  data["entry"].each do |entry|
     entry["changed_fields"].each do |field|
       exchange.publish(raw, :routing_key => field)
     end
